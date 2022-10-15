5b4f127dc6743edb6bf7596285ffe46e6a0ccb28
andrey.shavyrin <andrey.shavyrin@glowbyteconsulting.com>
2022-10-15 14:03:27 +0300

    upd

--- examples/homework1/linkedList.js
+++ examples/homework1/linkedList.js
@@ -9,36 +9,60 @@ list.insertLast(10);
 list.insertLast(16);
 
 for (const item of list) {
-    console.log(item); // 5 0 1 2 10 16
+    console.log(item);
+    // Link { value: 5 }
+    // Link { value: 0 }
+    // Link { value: 1 }
+    // Link { value: 2 }
+    // Link { value: 10 }
+    // Link { value: 16 }
 }
 
 for (const item of list.reverse()) {
-    console.log(item); // 16 10 2 1 0 5
+    console.log(item);
+    // Link { value: 16 }
+    // Link { value: 10 }
+    // Link { value: 2 }
+    // Link { value: 1 }
+    // Link { value: 0 }
+    // Link { value: 5 }
 }
 
 list.deleteFirst();
 
 for (const item of list) {
-    console.log(item); // 0 1 2 10 16
+    console.log(item);
+    // Link { value: 0 }
+    // Link { value: 1 }
+    // Link { value: 2 }
+    // Link { value: 10 }
+    // Link { value: 16 }
 }
 
 list.deleteLast();
 
 for (const item of list) {
-    console.log(item); // 0 1 2 10
+    console.log(item);
+    // Link { value: 0 }
+    // Link { value: 1 }
+    // Link { value: 2 }
+    // Link { value: 10 }
 }
 
 list.delete(2);
 
 for (const item of list) {
-    console.log(item); // 0 1 10
+    console.log(item);
+    // Link { value: 0 }
+    // Link { value: 1 }
+    // Link { value: 10 }
 }
 
 console.log(list.find(1)); // Link { value: 1 }
 
 const listOfNumbers = new LinkedList([1, 2, 3, 4]);
 for (const number of listOfNumbers) {
-    console.log(number);
+    console.log(number); // 1 2 3 4
     // Link { value: 1 }
     // Link { value: 2 }
     // Link { value: 3 }
